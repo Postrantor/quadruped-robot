9d7d41fbb121200bb1bb7811892a269c70022702
Your Name <you@example.com>
2023-07-31 08:09:08 -0700

    [dev_dog] fix keyboard

--- model-control/src/examples/example_keyboard/example_keyboard.cpp
+++ model-control/src/examples/example_keyboard/example_keyboard.cpp
@@ -9,16 +9,17 @@ int main(int argc, char **argv)
     qrTeleKeyboard *keyboard = new qrTeleKeyboard(nh);
     std::cout << "---------Keyboard start receving---------" << std::endl;
     std::cout << "You can use:\n"
-    // "'k'             to enable/disable control\n"
-    "'l'             to torque-stance\n"
-    "'j'             to change the gait\n"
-    "'u'             to position-stand up/down\n"
-    "'i'             to exit after sit down\n\n"
+                 "'k'             to enable/disable control\n"
+                 "'l'             to torque-stance\n"
+                 "'j'             to change the gait\n"
+                 "'u'             to position-stand up/down\n"
+                 "'i'             to exit after sit down\n\n"
 
-    "'w' 'a' 's' 'd' to control the robot's movement\n"
-    "'q' 'e'         to control the robot's rotation\n\n"
+                 "'w' 'a' 's' 'd' to control the robot's movement\n"
+                 "'q' 'e'         to control the robot's rotation\n\n"
 
-    "'Ctrl+C'        to exit keyboard_control" << std::endl;
+                 "'Ctrl+C'        to exit keyboard_control"
+              << std::endl;
     std::thread keyboardTh(&qrTeleKeyboard::run, keyboard);
     std::thread keyboardTh_default(&qrTeleKeyboard::run_default, keyboard);
     keyboardTh.join();
