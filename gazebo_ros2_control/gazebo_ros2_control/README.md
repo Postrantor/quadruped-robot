6e99041ba8c331bd8e3ed53b051522db6b3e6ac2
trantor <postrantor@gmail.com>
2024-03-07 00:05:24 +0800

    [sim] add a1_description and gazebo_template
    Signed-off-by: trantor <postrantor@gmail.com>

---
+++ gazebo_ros2_control/gazebo_ros2_control/README.md
@@ -0,0 +1,9 @@
+# Gazebo ros_control Interfaces
+
+This is a ROS 2 package for integrating the [ros2_control](https://github.com/ros-controls/ros2_control) controller architecture with the [Gazebo Classic](https://classic.gazebosim.org/) simulator.
+
+> 这是一个 ROS 2 软件包，用于将 [ros2_control](https://github.com/ros-controls/ros2_control) 控制器架构与 [Gazebo Classic](https://classic.gazebosim.org/) 模拟器集成在一起。
+
+This package provides a Gazebo plugin which instantiates a ros_control controller manager and connects it to a Gazebo model.
+
+> 该软件包提供了一个 Gazebo 插件，用于实例化 ros_control 控制器管理器并将其连接到 Gazebo 模型。
---
+++ gazebo_ros2_control/gazebo_ros2_control/include/gazebo_ros2_control/gazebo_ros2_control_plugin.hpp
@@ -0,0 +1,37 @@
+/**
+ * @brief
+ * @date 2024-03-04
+ * @copyright Copyright (c) 2024
+ */
+
+#ifndef GAZEBO_ROS2_CONTROL__GAZEBO_ROS2_CONTROL_PLUGIN_HPP_
+#define GAZEBO_ROS2_CONTROL__GAZEBO_ROS2_CONTROL_PLUGIN_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "controller_manager/controller_manager.hpp"
+
+#include "gazebo/common/common.hh"
+#include "gazebo/physics/Model.hh"
+
+namespace gazebo_ros2_control {
+class GazeboRosControlPrivate;
+
+class GazeboRosControlPlugin : public gazebo::ModelPlugin {
+public:
+  GazeboRosControlPlugin();
+
+  ~GazeboRosControlPlugin();
+
+  // Overloaded Gazebo entry point
+  void Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf) override;
+
+private:
+  /// Private data pointer
+  std::unique_ptr<GazeboRosControlPrivate> impl_;
+};
+}  // namespace gazebo_ros2_control
+
+#endif  // GAZEBO_ROS2_CONTROL__GAZEBO_ROS2_CONTROL_PLUGIN_HPP_
