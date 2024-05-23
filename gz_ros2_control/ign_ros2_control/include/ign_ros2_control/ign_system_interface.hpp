57e0f759a100551d3397a46001cfa04bf19de98d
trantor <postrantor@gmail.com>
2024-05-23 22:27:46 +0800

    [gazebo] update gz_ros2_control
    Signed-off-by: trantor <postrantor@gmail.com>

--- gz_ros2_control/ign_ros2_control/include/ign_ros2_control/ign_system_interface.hpp
+++ gz_ros2_control/ign_ros2_control/include/ign_ros2_control/ign_system_interface.hpp
@@ -1,15 +1,24 @@
-/**
- * @brief
- * @date 2024-03-04
- * @copyright Copyright (c) 2024
- */
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #ifndef IGN_ROS2_CONTROL__IGN_SYSTEM_INTERFACE_HPP_
 #define IGN_ROS2_CONTROL__IGN_SYSTEM_INTERFACE_HPP_
 
 #include <map>
-#include <string>
 #include <memory>
+#include <string>
 #include <vector>
 
 #include <ignition/gazebo/System.hh>
@@ -19,7 +28,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 
-namespace ign_ros2_control {
+namespace ign_ros2_control
+{
 
 /// \brief This class allows us to handle flags easily, instead of using strings
 ///
@@ -38,44 +48,32 @@ namespace ign_ros2_control {
 /// foo & POSITION -> True  // Check if position is active in the flag
 /// foo & VELOCITY -> False  // Check if velocity is active in the flag
 
-template <class ENUM, class UNDERLYING = typename std::underlying_type<ENUM>::type>
-class SafeEnum {
+template<class ENUM, class UNDERLYING = typename std::underlying_type<ENUM>::type>
+class SafeEnum
+{
 public:
-  SafeEnum() : mFlags(0) {}
-  explicit SafeEnum(ENUM singleFlag) : mFlags(singleFlag) {}
-  SafeEnum(const SafeEnum& original) : mFlags(original.mFlags) {}
+  SafeEnum()
+  : mFlags(0) {}
+  explicit SafeEnum(ENUM singleFlag)
+  : mFlags(singleFlag) {}
+  SafeEnum(const SafeEnum & original)
+  : mFlags(original.mFlags) {}
 
-  SafeEnum& operator|=(ENUM addValue) {
-    mFlags |= addValue;
-    return *this;
-  }
-  SafeEnum operator|(ENUM addValue) {
-    SafeEnum result(*this);
-    result |= addValue;
-    return result;
-  }
-  SafeEnum& operator&=(ENUM maskValue) {
-    mFlags &= maskValue;
-    return *this;
-  }
-  SafeEnum operator&(ENUM maskValue) {
-    SafeEnum result(*this);
-    result &= maskValue;
-    return result;
-  }
-  SafeEnum operator~() {
-    SafeEnum result(*this);
-    result.mFlags = ~result.mFlags;
-    return result;
-  }
-  explicit operator bool() { return mFlags != 0; }
+  SafeEnum & operator|=(ENUM addValue) {mFlags |= addValue; return *this;}
+  SafeEnum operator|(ENUM addValue) {SafeEnum result(*this); result |= addValue; return result;}
+  SafeEnum & operator&=(ENUM maskValue) {mFlags &= maskValue; return *this;}
+  SafeEnum operator&(ENUM maskValue) {SafeEnum result(*this); result &= maskValue; return result;}
+  SafeEnum operator~() {SafeEnum result(*this); result.mFlags = ~result.mFlags; return result;}
+  explicit operator bool() {return mFlags != 0;}
 
 protected:
   UNDERLYING mFlags;
 };
 
 // SystemInterface provides API-level access to read and command joint properties.
-class IgnitionSystemInterface : public hardware_interface::SystemInterface {
+class IgnitionSystemInterface
+  : public hardware_interface::SystemInterface
+{
 public:
   /// \brief Initialize the system interface
   /// param[in] model_nh Pointer to the ros2 node
@@ -85,18 +83,19 @@ public:
   /// param[in] _ecm Entity-component manager.
   /// param[in] update_rate controller update rate
   virtual bool initSim(
-      rclcpp::Node::SharedPtr& model_nh,
-      std::map<std::string, ignition::gazebo::Entity>& joints,
-      const hardware_interface::HardwareInfo& hardware_info,
-      ignition::gazebo::EntityComponentManager& _ecm,
-      int& update_rate) = 0;
+    rclcpp::Node::SharedPtr & model_nh,
+    std::map<std::string, ignition::gazebo::Entity> & joints,
+    const hardware_interface::HardwareInfo & hardware_info,
+    ignition::gazebo::EntityComponentManager & _ecm,
+    int & update_rate) = 0;
 
   // Methods used to control a joint.
-  enum ControlMethod_ {
-    NONE = 0,
-    POSITION = (1 << 0),
-    VELOCITY = (1 << 1),
-    EFFORT = (1 << 2),
+  enum ControlMethod_
+  {
+    NONE      = 0,
+    POSITION  = (1 << 0),
+    VELOCITY  = (1 << 1),
+    EFFORT    = (1 << 2),
   };
 
   typedef SafeEnum<enum ControlMethod_> ControlMethod;
