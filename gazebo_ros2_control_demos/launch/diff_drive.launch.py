af13989aee84c37069f8f4229da3e4629b629a86
trantor <postrantor@gmail.com>
2024-05-26 19:56:38 +0800

    [sim] add rviz2 to launch file
    Signed-off-by: trantor <postrantor@gmail.com>

--- gazebo_ros2_control_demos/launch/diff_drive.launch.py
+++ gazebo_ros2_control_demos/launch/diff_drive.launch.py
@@ -1,12 +1,22 @@
-import os
+import os, xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, GroupAction, TimerAction, DeclareLaunchArgument
+from launch.substitutions import Command, PathJoinSubstitution
+from launch.substitutions.launch_configuration import LaunchConfiguration
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-import xacro
 
+# ARGUMENTS = [
+#     DeclareLaunchArgument('use_sim_time', default_value='false',
+#                           choices=['true', 'false'],
+#                           description='use_sim_time'),
+#     DeclareLaunchArgument('robot_name', default_value='a1_description',
+#                           description='robot name'),
+#     DeclareLaunchArgument('namespace', default_value=LaunchConfiguration('robot_name'),
+#                           description='robot namespace'),
+# ]
 
 def generate_launch_description():
     gazebo = IncludeLaunchDescription(
@@ -14,42 +24,50 @@ def generate_launch_description():
             get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
     )
 
-    gazebo_ros2_control_demos_path = os.path.join(
-        get_package_share_directory('gazebo_ros2_control_demos'))
-
-    xacro_file = os.path.join(gazebo_ros2_control_demos_path,
-                              'urdf',
-                              'test_diff_drive.xacro.urdf')
-
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    params = {'robot_description': doc.toxml()}
+    pkg_description = get_package_share_directory('gazebo_ros2_control_demos')
+    rviz2_config = PathJoinSubstitution([pkg_description, 'config', 'model.rviz'])
+    xacro_file = PathJoinSubstitution([pkg_description, 'urdf', 'robot.urdf'])
+    namespace = LaunchConfiguration('namespace')
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        parameters=[
+            {'use_sim_time': False},
+            {'robot_description': Command(['xacro', ' ', xacro_file, ' ',])},
+        ],
         output='screen',
-        parameters=[params]
     )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'diffbot'],
-                        output='screen')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'diffbot'],
+        output='screen')
 
     load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster'],
         output='screen'
     )
 
     load_diff_drive_base_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'diff_drive_base_controller'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'diff_drive_base_controller'],
         output='screen'
     )
 
-    return LaunchDescription([
+    # add rviz
+    robot_rviz = GroupAction([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz2_config],
+            parameters=[{'use_sim_time': False}],
+            output='screen')
+            ]
+        )
+
+    ld = LaunchDescription([
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=spawn_entity,
@@ -65,4 +83,7 @@ def generate_launch_description():
         gazebo,
         node_robot_state_publisher,
         spawn_entity,
+        robot_rviz,
     ])
+
+    return ld
